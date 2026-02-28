b4c01ecaf726e06176df18610624cef97972fd4b
M4xzbtw <dylanbren221@gmail.com>
2026-02-28 01:25:16 -0600

    Dylan

---
+++ WebApplicationAPP/Views/Laboratorios/Index.cshtml.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using WebApplicationAPP.Models;
+using WebApplicationAPP.Servicios;
+
+namespace WebApplicationAPP.Views.Laboratorios
+{
+    public class IndexModel : PageModel
+    {
+
+        private readonly InMemoryRepository _repository;
+
+        public List<Laboratorio> Laboratorios { get; set; }
+
+        public IndexModel()
+        {
+            _repository = new InMemoryRepository();
+        }
+
+        public void OnGet()
+        {
+            Laboratorios = _repository.GetLaboratorios();
+        }
+
+        public IActionResult OnPost(string Name, int Capacity, string Responsible)
+        {
+            var lab = new Laboratorio
+            {
+                Name = Name,
+                Capacity = Capacity,
+                Responsible = Responsible
+            };
+
+            _repository.AddLaboratorio(lab);
+
+            return RedirectToPage();
+
+        }
+
+        public IActionResult OnPostDelete(int id)
+        {
+            _repository.DeleteLaboratorio(id);
+
+            return RedirectToPage();
+        }
+
+
+    }
+}
